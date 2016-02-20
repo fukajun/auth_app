e61b21c7ee60ab723d416cab5b3cd0e9ef619ae1
J.Fukaya <fukajun.shark@gmail.com>
2016-02-21 00:25:18 +0900

    Update migration for authentications

--- db/migrate/20160220151505_create_authentications.rb
+++ db/migrate/20160220151505_create_authentications.rb
@@ -1,7 +1,16 @@
 class CreateAuthentications < ActiveRecord::Migration
   def change
     create_table :authentications do |t|
-
+      t.integer  :user_id,       null: false
+      t.string   :provider,      null: false
+      t.string   :uid,           null: false
+      t.string   :access_token,  null: false
+      t.string   :refresh_token
+      t.string   :secret
+      t.string   :screen_name,   null: false
+      t.string   :name,          null: false
+      t.string   :email
+      t.string   :image_url
       t.timestamps null: false
     end
   end
