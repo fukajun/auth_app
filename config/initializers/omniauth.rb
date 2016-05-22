8de723feb3f68eb028e8b7af9d59d0a9baf5abe0
J.Fukaya <fukajun.shark@gmail.com>
2016-05-22 17:51:10 +0900

    Setting omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :identity, :fields => [:email], :model => Identity
+  provider :identity, :fields => [:email], :model => Identity, on_failed_registration: IdentitiesController.action(:new)
+  OmniAuth.config.on_failure = Proc.new { |env|
+    OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+  }
 end
