8143f5e0888cb4b206700b817dc02f6d5eaf6f10
J.Fukaya <fukajun.shark@gmail.com>
2016-05-29 20:16:45 +0900

    Add sigin, signout, singup feature

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 feature 'Access home page' do
-  scenario '' do
-    visit '/'
-    expect(current_path).to eq '/'
-  end
-
   scenario '', js: true do
     visit '/'
     expect(current_path).to eq '/'
